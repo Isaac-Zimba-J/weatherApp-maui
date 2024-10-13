45484529ff9deb23c417e65a5b0770936053bace
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2024-10-13 23:54:56 +0200

    initial push

---
+++ WeatherApp/View/WeatherView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.View;
+
+public partial class WeatherView : ContentPage
+{
+    public WeatherView()
+    {
+        InitializeComponent();
+    }
+}
